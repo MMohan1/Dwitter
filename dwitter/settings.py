3957f1981bdc7d3a92652f48b542e65db821772e
manmohan <manmohan.sharma@edgenetworks.in>
2017-08-26 15:29:08 +0530

    Setting Up session for 24 hours

--- dwitter/settings.py
+++ dwitter/settings.py
@@ -123,9 +123,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "dwitter_app/static")
 ]
 STATIC_URL = '/static/'
-# TEMPLATE_DIRS = (
-#     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-#     # Always use forward slashes, even on Windows.
-#     # Don't forget to use absolute paths, not relative paths.
-#     os.path.join(BASE_DIR, 'dwitter/templates/'),
-# )
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
+SESSION_COOKIE_AGE = 24*60*60
