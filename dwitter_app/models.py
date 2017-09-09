a4d67051946c64db120802a7fcdc4deb957fb991
manmohan <manmohan.sharma@edgenetworks.in>
2017-09-09 10:20:15 +0530

    Models sechma changes

--- dwitter_app/models.py
+++ dwitter_app/models.py
@@ -4,33 +4,39 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 import hashlib
- 
- 
-class Dwitter(models.Model):
+
+
+class Dweet(models.Model):
     content = models.CharField(max_length=140, unique=True)
     user = models.ForeignKey(User)
     creation_date = models.DateTimeField(auto_now=True, blank=True)
- 
- 
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
-    follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
- 
+    # follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
+
     def gravatar_url(self):
         return "http://www.gravatar.com/avatar/%s?s=50" % hashlib.md5(self.user.email).hexdigest()
- 
- 
-User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
 
 
-class DwitterLike(models.Model):
-    dwitte = models.ForeignKey(Dwitter)
-    likes = models.ManyToManyField(User)
-    creation_date = models.DateTimeField(auto_now=True, blank=True)
+class Follow(models.Model):
+    follower = models.ForeignKey(User, related_name='follower')
+    following = models.ForeignKey(User, related_name='following')
+    follow_date = models.DateTimeField(auto_now=True)
 
 
-class DwitterComment(models.Model):
-    dwitte = models.ForeignKey(Dwitter)
-    comment_by = models.ManyToManyField(User)
+#User.follow = property(lambda u: Follow.objects.filter(user=u))
+
+
+class Likes(models.Model):
+    dwitte = models.ForeignKey(Dweet)
+    likes = models.ForeignKey(User)
+    creation_date = models.DateTimeField(auto_now=True)
+
+
+class Comments(models.Model):
+    dwitte = models.ForeignKey(Dweet)
+    comment_by = models.ForeignKey(User)
     comment = models.TextField()
-    creation_date = models.DateTimeField(auto_now=True, blank=True)
+    creation_date = models.DateTimeField(auto_now=True)
